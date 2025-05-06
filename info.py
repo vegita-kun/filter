7421d3887d94656ac8310417af64427ab80c8864
Mongodb-ai <mongodb703@gmail.com>
2025-05-06 11:19:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,12 +69,12 @@ STREAM_MODE = is_enabled((environ.get('STREAM_MODE', "False")), False)
 #verify site api and url
 IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
-VERIFY_URL = environ.get('VERIFY_URL', 'modijiurl.com')
-VERIFY_API = (environ.get('VERIFY_API', '266f12e727250353b7108b3ffc9d40af4645a646'))
+VERIFY_URL = environ.get('VERIFY_URL', 'Tfushorty.com')
+VERIFY_API = (environ.get('VERIFY_API', '0efb6a1a544567f7b967bb3e275b2c0c1092c818'))
 
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "600"))
-VERIFY_URL2 = environ.get('VERIFY_URL2', 'Tfushorty.com')
-VERIFY_API2 = (environ.get('VERIFY_API2', '0efb6a1a544567f7b967bb3e275b2c0c1092c818'))
+VERIFY_URL2 = environ.get('VERIFY_URL2', '')
+VERIFY_API2 = (environ.get('VERIFY_API2', ''))
  
 THIRD_VERIFY_GAP = int(environ.get('THIRD_VERIFY_GAP', "600"))
 VERIFY_URL3 = environ.get('VERIFY_URL3', '')
