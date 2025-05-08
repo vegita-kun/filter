6168eead4ba53d3e4477d27c8c6001274b9db6a8
Mongodb-ai <mongodb703@gmail.com>
2025-05-08 12:00:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002199295496'))
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Safariibot:GVSKmNuf0e4UWuSh@cluster0.jvwydhc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://raj:bot@cluster0.utq4u4j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Safaribotts')
 
