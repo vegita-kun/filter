b20bfd677ec0f19886a86da9543250455445874c
Mongodb-ai <mongodb703@gmail.com>
2025-05-09 19:38:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002199295496'))
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://thelevii09:j7l6ye78YQdCVwf1@cluster0.cgpepyi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mongodb703:YsY3DXVWt914ESpg@cluster0.78sqf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
